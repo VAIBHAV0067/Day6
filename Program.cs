68bd13561558361e29a9b1b5513064b642ebd3cb
tutorial6 <manapure.vaibhav542@gmail.com>
2022-03-05 18:34:24 +0530

    Day6 logical Program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseNo.RevNo();
+            //PerfectNumber.CheckPerfectNumber();
+            //SwapTwoNumber.SwapNumber();
+
+        }
+    }
+}
---
+++ SwapTwoNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    public class SwapTwoNumber
+    {
+        public static void SwapNumber()
+        {
+            int a = 5, b = 10;
+          //  Console.WriteLine("Before swap a = " + a = "b =" + b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+
+           // Console.WriteLine("After swap a = " + a = "b = " + b);
+
+        }
+    }
+}
